3498f202eeb2a23b5f1e00e1d57a40c6df6a2b00
FXC-ai <fx.coindreau@gmail.com>
2024-07-24 14:32:59 +0200

    les variables

--- jeu_du_plus_ou_du_moins/src/main.rs
+++ jeu_du_plus_ou_du_moins/src/main.rs
@@ -4,27 +4,30 @@ use std::cmp::Ordering;
 
 fn main() 
 {
-    println!("Proposer un nombre :");
-    let mut supposition = String::new();
-    
     let nombre_secret = rand::thread_rng().gen_range(1..101);
-    println!("Le nombre secret est : {}", nombre_secret);
-    
-    io::stdin().read_line(&mut supposition).expect("Echec de la lecture de l'entrée utilisateur.");
-    
-    let supposition: u32 = supposition.trim().parse().expect("Veuillez entrer un nombre !");
+    // println!("Le nombre secret est : {}", nombre_secret);
     
     loop
     {
+        println!("Proposer un nombre :");
+        let mut supposition = String::new();
+        
+        io::stdin().read_line(&mut supposition).expect("Echec de la lecture de l'entrée utilisateur.");
+        
+        let supposition: u32 = match supposition.trim().parse()
+        {
+            Ok(nb) => nb,
+            Err(_) => continue,
+        };
+        
         match supposition.cmp(&nombre_secret)
         {
             Ordering::Less => println!("C'est plus !"),
             Ordering::Greater => println!("C'est moins !"),
-            Ordering::Equal => println!("Vous avez gagné !"),
+            Ordering::Equal => {
+                println!("Vous avez gagné !");
+                break;
+            }
         }
     }
-    
-
-    println!("Votre nombre est : {}", supposition);
-
 }
