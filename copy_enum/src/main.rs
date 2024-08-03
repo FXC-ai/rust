dd9cf5cabaebf2e7595f8df7cfdebdc9765be383
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-08-03 16:25:19 +0200

    debut modules

--- copy_enum/src/main.rs
+++ copy_enum/src/main.rs
@@ -1,41 +1,17 @@
-#[derive(Debug)]
-enum Trois
-{
-	EnNombre(i32),
-	EnStr(String),
-	Autre,
-}
-
-fn plus_un (x : Option<i32>) -> Option<i32>
-{
-	match x
-	{
-		None => None,
-		Some(p) => Some(p+1),
-	}
-}
-
 
 fn main ()
 {
-	let cinq = plus_un(Some(5));
-	println!("{:?}", cinq);
-	let nul = plus_un(None);
-	println!("{:?}", nul);
+	let une_valeur_u8 : Option<u8> = Some(3);
 
-
-	println!("t {:?}", Trois::EnNombre(42));
-
-	let var_test = "trois en nombre";
-
-	let result_match = match var_test
+	match une_valeur_u8 {
+		Some(max) => println!("{}", max),
+		_ => (),
+	}
+	if let Some(qqchose) = une_valeur_u8 {
+		println!("{}", qqchose);
+	}
+	else
 	{
-		"trois en nombre" => Trois::EnNombre(3),
-		"trois en chiffre" => Trois::EnStr(String::from("trois")),
-		_ => Trois::Autre,
-	};
-
-
-	println!("{:?}", result_match);
-
+		()
+	}
 }
