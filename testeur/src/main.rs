fc36e8d1ed3db0a075c6d7d2ded4f7a4ab6b9336
FXC-ai <fx.coindreau@gmail.com>
2024-08-08 16:03:39 +0200

    fin de la gestion des erreurs

--- testeur/src/main.rs
+++ testeur/src/main.rs
@@ -1,14 +1,26 @@
-
-
+use std::fs::File;
+use std::io;
 
 fn main()
 {
-    // panic!("ererr");
-    let v = vec![1,2,3,4];
-    v[1];
-    v[1000];
-
+    let f = ouvrir_fichier;
+    let g = ouvrir_fichier_1;
 }
 
+fn ouvrir_fichier() -> Result<File, io::Error>
+{
+    let f = File::open("hello.txt");
 
+    match f
+    {
+        Ok(fichier) => Ok(fichier),
+        Err(e) => return Err(e),
+    }
+}
 
+fn ouvrir_fichier_1() -> Result<File, io::Error>
+{
+    let mut f = File::open("hello.txt")?;
+    let ff : Result<File, io::Error> = Ok(f);
+    ff
+}
